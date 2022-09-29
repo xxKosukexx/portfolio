46c45ece9333f7b5b681dadf7c5d08f52dbc993b
xxKosukexx <clover1111bag@gmail.com>
2022-09-29 19:49:52 +0900

    tailwind css installation

--- front/app/src/components/view/layout.tsx
+++ front/app/src/components/view/layout.tsx
@@ -5,10 +5,10 @@ import type { ReactNode } from "react";
 
 export default function Layout({ children }: {children: ReactNode}) {
   return (
-    <>
+    <div className=''>
       <Header />
-      {children}
+      <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
